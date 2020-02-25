c0d00f201668219f08d959d2d84b3c289bd91f4f
Kaahan <kaahan15@gmail.com>
2020-02-24 20:43:41 -0600

    Load game from URL and create two helper functions to check for URL validity

--- src/main/java/student/Main.java
+++ src/main/java/student/Main.java
@@ -6,6 +6,8 @@ import student.adventure.Layout;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.rmi.server.ExportException;
 import java.util.Scanner;
@@ -18,18 +20,52 @@ public class Main {
      */
     public static void main(String[] args) throws IOException {
 //        URL url = new URL("https://courses.grainger.illinois.edu/cs126/sp2020/resources/siebel.json");
-//        Layout layout = new ObjectMapper().readValue(url, Layout.class);
+        Layout layout = new Layout();
         File file;
         if (args == null || args.length == 0) {
             file = new File("src/main/resources/siebel.json");
         } else {
             file = new File(args[0]);
-            if (!(file).exists()) {
+            if (isValidUrl(args[0])) {
+                layout = parseUrlForJsonFile(args[0]);
+            } else if (!(file).exists()) {
                 System.out.println("This file does not exist, sorry!");
+            } else {
+                layout = new ObjectMapper().readValue(file, Layout.class);
             }
         }
-        Layout layout = new ObjectMapper().readValue(file, Layout.class);
+
         Adventure.continueGame(layout);
-        //TODO check if JSON files have null rooms, directions etc
     }
+
+    /**
+     *
+     * @param userInput
+     * @return
+     * @throws IOException
+     */
+    private static boolean isValidUrl(String userInput) throws IOException {
+        URL url;
+
+        try {
+            url = new URL(userInput);
+        } catch (MalformedURLException e) {
+            return false;
+        }
+
+        HttpURLConnection huc = (HttpURLConnection) url.openConnection();
+
+        return huc.getResponseCode() == HttpURLConnection.HTTP_OK;
+    }
+
+    /**
+     *
+     * @return
+     */
+    private static Layout parseUrlForJsonFile(String userInputUrl) throws IOException {
+        URL url = new URL(userInputUrl);
+        Layout layout = new ObjectMapper().readValue(url, Layout.class);
+        return layout;
+    }
+
 }
